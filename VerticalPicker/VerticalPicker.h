201d8dbb43c612a359a9cbff3be3188623efc16a
Hugo Perez <umwelt.hugo@gmail.com>
2020-02-27 16:44:48 +0100

    Initial Commit

---
+++ VerticalPicker/VerticalPicker.h
@@ -0,0 +1,19 @@
+//
+//  VerticalPicker.h
+//  VerticalPicker
+//
+//  Created by Hugo Perez on 27/02/2020.
+//  Copyright © 2020 WodPod. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for VerticalPicker.
+FOUNDATION_EXPORT double VerticalPickerVersionNumber;
+
+//! Project version string for VerticalPicker.
+FOUNDATION_EXPORT const unsigned char VerticalPickerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <VerticalPicker/PublicHeader.h>
+
+
