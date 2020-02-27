11b8a0a6179aab3c7be2fdfb07d1820e2da39aa3
Hugo Perez <umwelt.hugo@gmail.com>
2020-02-27 17:39:19 +0100

    Public Class

--- VerticalPicker/SinglePickViewController.swift
+++ VerticalPicker/SinglePickViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class SinglePickViewController: UIViewController {
+public class SinglePickViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
